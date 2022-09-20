1138300ab086200fd64e459eb6ac2fe1ed969ac8
paunicolae <pa0pau14@yahoo.com>
2022-09-21 04:11:49 +0800

    added layout

--- resources/views/userbill.blade.php
+++ resources/views/userbill.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.sidebar')
 
 
 <style>
@@ -67,17 +67,75 @@ th{
 }
 
 
+/* Sidebar */
+.sidebar {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  padding: 58px 0 0; /* Height of navbar */
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
+  z-index: 600;
+  background-color: transparent;
+  shadow: none;
+}
+
+.sidebar .active {
+  border-radius: 5px;
+  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+}
+
+
+nav#sidebarMenu{
+  width:200px;
+}
+
+.position-sticky {
+  position: relative;
+  top: 0;
+  width:200px;
+  padding-top: 1rem;
+  overflow-x: hidden;
+  overflow-y: auto; /* Scrollable contents if viewport is shorter than content. */
+}
+
+
+ /* media query */
+@media screen and (max-width: 480px){
+
+p.current{
+  font-size: 1.5rem;
+}
+
+}
+
 @media screen and (min-width: 768px) and (max-width:991px){
 #position{
   margin-left: 10rem;
 }
 }
-
-
 </style>
 
 
 @section('content')
+ <!-- Sidebar -->
+ <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse" style="box-shadow: none;">
+  <div class="position-sticky">
+    <div class="list-group list-group-flush mx-3 mt-4">
+
+      <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+      ><i class="fas fa-money-bill fa-fw me-3"></i><span>Create Bill</span></a
+    >
+      <a href="{{url('/admin/data')}}" class="list-group-item list-group-item-action py-2 ripple"
+      ><i class="fas fa-users fa-fw me-3"></i><span>Customers</span></a
+    >
+      <a href="#" class="list-group-item list-group-item-action py-2 ripple"
+        ><i class="fas fa-chart-bar fa-fw me-3"></i><span>Bills</span></a
+      >
+    </div>
+  </div>
+</nav>
+<!-- Sidebar -->
 <section class="section">
 <div class="container mt-5 pt-5 mb-5 pb-5" >
     <div class="row">
