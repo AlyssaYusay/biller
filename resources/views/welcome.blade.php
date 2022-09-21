40acf44f3625975e3cbda0a35e404bdcb12a3316
paunicolae <pa0pau14@yahoo.com>
2022-09-22 04:08:49 +0800

    added scroll to top

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -213,7 +213,27 @@ padding-bottom: 5rem;
     filter: brightness(80%)
 }
 
+/* scroll to top */
+#myBtn {
+  display: none;
+  position: fixed;
+  bottom: 20px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: 2px white;
+  outline: none;
+  background-color: orange;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+  text-shadow: -1px 0 white, 0 1px white, 1px 0 white, 0 -1px white;
+}
 
+#myBtn:hover {
+  filter: brightness(80%)
+}
 input.form-control{
     width: 90%;
     margin-left:2rem;
@@ -385,6 +405,7 @@ input.form-control{
  
     </head>
  <body>
+    <button onclick="topFunction()" id="myBtn" title="Go to top"><span class="iconify" data-icon="bytesize:arrow-top" style="color: white;"></span></button>
 <section class="hero-section">
 
         <div class="flex-center position-ref full-height">
@@ -514,6 +535,7 @@ input.form-control{
 {{-- end of login container --}}
     </div>
 </section>
+
 
  <section class="list">
     <h1 class="text-center text-dark mt-5 mb-5">Have the power to access your Meralca account wherever, whenever with</h1>
@@ -688,7 +710,27 @@ input.form-control{
 
 
 
-        
+ <script>
+    // Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>       
 <script type='text/javascript' src='https://cdn.jsdelivr.net/npm/emailjs-com@3/dist/email.min.js'></script>
 <script type='text/javascript'>
 (function(){
